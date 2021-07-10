301aba11e31bc6516dd55761d4644144909ac5b6
Tulio Colmanetti <tulio.colmanetti@gmail.com>
2021-07-09 23:18:59 -0300

    Added Person entity and related lombok annotations

---
+++ src/main/java/com/projects/dev/tulio/simplerescrudapi/model/Person.java
@@ -0,0 +1,28 @@
+package com.projects.dev.tulio.simplerescrudapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String cpf;
+
+    private LocalDate birthDate;
+
+    private List<Phone> phones;
+}
