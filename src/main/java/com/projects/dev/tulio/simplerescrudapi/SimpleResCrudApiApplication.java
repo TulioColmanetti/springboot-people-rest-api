d4b76f1d5cf4009cb69e9ea576e10c21772f0e92
Tulio Colmanetti <tulio.colmanetti@gmail.com>
2021-07-09 22:44:59 -0300

    First commit

---
+++ src/main/java/com/projects/dev/tulio/simplerescrudapi/SimpleResCrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.projects.dev.tulio.simplerescrudapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleResCrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleResCrudApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projects/dev/tulio/simplerescrudapi/SimpleResCrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.dev.tulio.simplerescrudapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleResCrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
